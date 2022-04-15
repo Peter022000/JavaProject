3399f1310d121ce1a1381bccfb3819a5b7cf59db
Makolojka <marcinkrol1@onet.pl>
2022-04-15 14:43:15 +0200

    shop_and_eq_cosmetics
    cosmetics

--- src/main/java/com/example/javaproject/EquipmentController.java
+++ src/main/java/com/example/javaproject/EquipmentController.java
@@ -26,4 +26,76 @@ public class EquipmentController {
 
         borderPane.setTop(topView);
     }
+<<<<<<< Updated upstream
+=======
+
+    @FXML
+    private void goBackToMenu(ActionEvent event) throws IOException, SQLException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("menu-view.fxml"));
+        Parent root = loader.load();
+
+        MenuController menuController = loader.getController();
+
+        menuController.setUserData(userData);
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void loadTable() throws SQLException {
+        ObservableList<Item> item = DatabaseConnectv2.getItems(userData.getUid());
+        tableName.setCellValueFactory(new PropertyValueFactory<Item,String>("name"));
+        tableValue.setCellValueFactory(new PropertyValueFactory<Item,String>("value"));
+        tableWeight.setCellValueFactory(new PropertyValueFactory<Item,String>("weight"));
+        tableDescription.setCellValueFactory(new PropertyValueFactory<Item,String>("description"));
+        tableAmount.setCellValueFactory(new PropertyValueFactory<Item,String>("amount"));
+        TableAction.setCellFactory(cellFactory);
+
+        moneyLabel.setText(String.format("%.2f", userData.getMoney()));
+
+        tableView.setItems(item);
+        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+    }
+
+    Callback<TableColumn<Item, String>, TableCell<Item, String>> cellFactory
+            = //
+            new Callback<TableColumn<Item, String>, TableCell<Item, String>>() {
+                @Override
+                public TableCell call(final TableColumn<Item, String> param) {
+                    final TableCell<Item, String> cell = new TableCell<Item, String>() {
+
+                        final Button btn = new Button("Discard");
+
+                        @Override
+                        public void updateItem(String item, boolean empty) {
+                            super.updateItem(item, empty);
+                            if (empty) {
+                                setGraphic(null);
+                                setText(null);//"-fx-background-color:" + colorString + "; -fx-fill:" + colorString+";"
+                            } else {
+                                btn.setOnAction(event -> {
+                                    int iid = getTableView().getItems().get(getIndex()).getIid();
+                                    try {
+                                        DatabaseConnectv2.deleteItemFromEquipment(userData.getUid(),iid);
+                                        loadTable();
+                                    } catch (SQLException e) {
+                                        e.printStackTrace();
+                                    }
+                                });
+                                setGraphic(btn);
+                                setText(null);
+                                btn.setStyle("-fx-background-color:\n" +
+                                        "                        #750a0e,\n" +
+                                        "                        linear-gradient(#ec2127, #bc1016);\n" +
+                                        "    -fx-text-fill: white;\n" +
+                                        "    -fx-background-radius: 10; -fx-font-size: 10;");
+                            }
+                        }
+                    };
+                    return cell;
+                }
+            };
+>>>>>>> Stashed changes
 }
--- src/main/java/com/example/javaproject/ShopController.java
+++ src/main/java/com/example/javaproject/ShopController.java
@@ -38,4 +38,78 @@ public class ShopController {
         this.shop = shop;
         shopLabel.setText(this.shop);
     }
+<<<<<<< Updated upstream
+=======
+
+    @FXML
+    void goBackToMenu(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("menu-view.fxml"));
+        Parent root = loader.load();
+
+        MenuController menuController = loader.getController();
+
+        menuController.setUserData(userData);
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    private void loadTable() throws SQLException {
+        ObservableList<Item> item = DatabaseConnectv2.getShopItems(sid);
+        tableName.setCellValueFactory(new PropertyValueFactory<Item,String>("name"));
+        tableValue.setCellValueFactory(new PropertyValueFactory<Item,String>("value"));
+        tableWeight.setCellValueFactory(new PropertyValueFactory<Item,String>("weight"));
+        tableDescription.setCellValueFactory(new PropertyValueFactory<Item,String>("description"));
+        tableAmount.setCellValueFactory(new PropertyValueFactory<Item,String>("amount"));
+        TableAction.setCellFactory(cellFactory);
+
+        moneyLabel.setText(String.format("%.2f", userData.getMoney()));
+
+        tableView.setItems(item);
+    }
+
+    Callback<TableColumn<Item, String>, TableCell<Item, String>> cellFactory
+            = //
+            new Callback<TableColumn<Item, String>, TableCell<Item, String>>() {
+                @Override
+                public TableCell call(final TableColumn<Item, String> param) {
+                    final TableCell<Item, String> cell = new TableCell<Item, String>() {
+
+                        final Button btn = new Button("Buy");
+
+                        @Override
+                        public void updateItem(String item, boolean empty) {
+                            super.updateItem(item, empty);
+                            if (empty) {
+                                setGraphic(null);
+                                setText(null);
+                            } else {
+                                btn.setOnAction(event -> {
+                                    int iid = getTableView().getItems().get(getIndex()).getIid();
+                                    try {
+                                        float money = DatabaseConnectv2.buyItemFromEquipment(sid,userData.getUid(),iid, userData.getMoney());
+                                        userData.setMoney(money);
+                                        loadTable();
+                                    } catch (SQLException e) {
+                                        e.printStackTrace();
+                                    }
+                                });
+                                setGraphic(btn);
+                                setText(null);
+                                btn.setStyle("-fx-background-color:\n" +
+                                        "                        #750a0e,\n" +
+                                        "                        linear-gradient(#ec2127, #bc1016);\n" +
+                                        "    -fx-text-fill: white;\n" +
+                                        "    -fx-background-radius: 10; -fx-font-size: 10;");
+                            }
+                        }
+                    };
+                    return cell;
+                }
+            };
+
+>>>>>>> Stashed changes
 }
