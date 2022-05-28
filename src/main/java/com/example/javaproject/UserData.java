2a6b44cc962ebb36208ecae32695d091c3a678a2
Piotr Duda <piotr.duda00@gmail.com>
2022-05-28 15:28:31 +0200

    Documentation final

--- src/main/java/com/example/javaproject/UserData.java
+++ src/main/java/com/example/javaproject/UserData.java
@@ -1,8 +1,18 @@
 package com.example.javaproject;
 
+/**
+ * Funkcja przechowująca dane użytkownika
+ */
+
 public class UserData {
     private int uid;
     private float money;
+
+    /**
+     * Konstruktor tworzący obiekt zawierający informacje o użytkowniku
+     * @param uid id użytkownika
+     * @param money pieniądze użytkownika
+     */
 
     public UserData(int uid, float money)
     {
@@ -10,22 +20,33 @@ public class UserData {
         this.money = money;
     }
 
+    /**
+     * Funkcja zwraca id użytkownika
+     * @return id użytkownika
+     */
+
     public int getUid() {
         return uid;
     }
 
-    public void setUid(int uid) {
-        this.uid = uid;
-    }
+    /**
+     * Funkcja zwracająca pieniądze użytkownika
+     * @return pieniądze użytkownika
+     */
 
     public float getMoney() {
         return money;
     }
 
+    /**
+     * Funkcja ustawiająca pieniądze użytkownika
+     * @param money
+     */
+
     public void setMoney(float money) {
         this.money = money;
     }
-
+    
     @Override
     public String toString() {
         return "UserData{" +
